e801a2c4aa4c69f1de78172125e45b27011758bf
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-16 12:53:39 +0200

    added explicative text

--- script.js
+++ script.js
@@ -4,21 +4,26 @@
     let textArea = document.getElementById('input_text');
     let charsCounter = document.getElementById('counter');
 
+    // initially hides the characters counter
     charsCounter.style.display = 'none';
     
+    // when the text area receives focus
     textArea.addEventListener('focus', function() {
         charsCounter.style.display = 'block';
         charsCounter.innerHTML = 'Characters left: <b>' + charsLeft() + '</b>';
     });
     
+    // when an input occurs in the text area
     textArea.addEventListener('input', function() {
         charsCounter.innerHTML = 'Characters left: <b>' + charsLeft() + '</b>';
     });
-
+    
+    // when the text area loses focus
     textArea.addEventListener('blur', function() {
         charsCounter.style.display = 'none';
     });
 
+    // function to return the number of characters left
     function charsLeft() {
         let charsInput = textArea.value.length;
         return 155 - charsInput;
