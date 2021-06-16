5ff4e8def8a03773b5ac063686a6c6ff69b3d460
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-16 12:08:13 +0200

    added selectors for textarea and character counter elements

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 (function() {
     console.warn('Script correctly linked to web page');
+
+    let textArea = document.getElementById('input_text');
+    let charsCounter = document.getElementById('counter');
 })();
