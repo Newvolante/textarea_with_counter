9c7645e2b9cec9a401635ec6fb158de9e6f4d4a1
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-16 13:20:08 +0200

    now the number of chars left is red when lesser than 25

--- script.js
+++ script.js
@@ -10,22 +10,33 @@
     // when the text area receives focus
     textArea.addEventListener('focus', function() {
         charsCounter.style.display = 'block';
-        charsCounter.innerHTML = 'Characters left: <b>' + charsLeft() + '</b>';
+        charsCounter.innerHTML = 'Characters left: <span id="number"><b>' + charsLeft() + '</b></span>';
+        numberColor();
     });
     
     // when an input occurs in the text area
     textArea.addEventListener('input', function() {
-        charsCounter.innerHTML = 'Characters left: <b>' + charsLeft() + '</b>';
+        charsCounter.innerHTML = 'Characters left: <span id="number"><b>' + charsLeft() + '</b></span>';
+        numberColor();
     });
     
     // when the text area loses focus
     textArea.addEventListener('blur', function() {
         charsCounter.style.display = 'none';
     });
-
+    
     // function to return the number of characters left
     function charsLeft() {
         let charsInput = textArea.value.length;
         return 155 - charsInput;
+    }
+
+    // changes the color of the number of chars left
+    function numberColor() {
+        let num = charsLeft();
+
+        if (num <= 25) {
+            document.getElementById('number').style.color = 'red';
+        }
     }
 })();
